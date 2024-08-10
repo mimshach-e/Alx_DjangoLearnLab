1f5f3ca864492284027ab0cdd10c0ef559f4daae
Ernest Mimshach <mimshach.e@gmail.com>
2024-08-10 11:19:30 +0000

    made changes to the admin.py file of the app

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("author", "publication_year")
+    search_fields = ("title", "author__name")
+    
+
+# grouping fields in the book form using fieldsets
+    fieldsets = (
+    (None, {
+        'fields': ('title', 'author')
+    }),
+    ('Additional Info', {
+        'fields': ('publication_year', 'isbn')
+    }),
+)
+    
+admin.site.register(Book, BookAdmin)
+
