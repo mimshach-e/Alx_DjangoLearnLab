863b73e6db69d8d9473442f98beeeefd2b03114c
Ernest Mimshach <mimshach.e@gmail.com>
2024-08-16 20:13:19 +0000

    made changes to the 'query_samples.py' file

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,23 +1,26 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author.
-author = Author.objects.get(id=1)
-all_books_by_author = author.books.all()
-
-print("All Books by the author:")
-for book in all_books_by_author:
-    print(book.title)
+def Books_By_Author():
+    author = Author.objects.get(id=1)
+    books_by_author = author.books.all()
+    
+    print("All Books by the author:")
+    for book in books_by_author:
+        print(book.title)
 
 # List all books in a library.
-library = Library.objects.get(id=1)
-all_books_in_library = library.books.all()
-
-print("All Books in the Library")
-for book in all_books_in_library:
-    print(book.title)
+def Books_in_Library(library_name):
+    library = Library.objects.get(name=library_name)
+    books_in_library = library.books.all()
+    
+    print("All Books in the Library")
+    for book in books_in_library:
+        print(book.title)
 
 # Retrieve the librarian for a library.
-library = Library.objects.get(id=1)
-librarian = library.librarian
+def Librarian_for_Library():
+    library = Library.objects.get(id=1)
+    librarian = library.librarian
 
-print(f"The Librarian for the library is: {librarian.name}")
+    print(f"The Librarian for the library is: {librarian.name}")
