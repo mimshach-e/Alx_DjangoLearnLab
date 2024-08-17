061243fa4f1f8065fe7c7858427a96a4a7238aa8
Ernest Mimshach <mimshach.e@gmail.com>
2024-08-17 17:15:25 +0000

    made changes to the urls.py and views.py files in the relationship_app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import Library
 
 # Create your views here.
 # Function-Based View FBV
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request, 'relationship_app/list_books.html', context)
