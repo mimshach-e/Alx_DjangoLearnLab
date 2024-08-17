b1c578d6e361892ff378cb4befaedb17da810521
Ernest Mimshach <mimshach.e@gmail.com>
2024-08-17 23:58:43 +0000

    made changes to url file in app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,9 +8,10 @@ from . import views
 urlpatterns = [
     path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     #path('logout/', custom_logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('accounts/profile/', TemplateView.as_view(template_name = 'relationship_app/profile.html'), name='profile'),
 ]
+
