5bcbd3f9d1721f5dc6a09fe839586d5d38c2e48d
Ernest Mimshach <mimshach.e@gmail.com>
2024-08-31 21:18:22 +0000

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('api-auth-token/', views.obtain_auth_token, name='api_token_auth')
 ]
