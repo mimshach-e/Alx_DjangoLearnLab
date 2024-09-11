6f58a1c73794393729b75d07494aebacbbd3a529
Ernest Mimshach <mimshach.e@gmail.com>
2024-09-11 18:56:17 +0000

    made changes to settings.py file

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'root',
+        'PASSWORD': 'RichMims@1607',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        
     }
 }
 
