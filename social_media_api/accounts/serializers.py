565b0457ac3e581f7b3f405a5bd0922d4880de28
Ernest Mimshach <mimshach.e@gmail.com>
2024-09-18 13:58:40 +0000

    made changes in the serializer.py file and views.py file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,44 +2,39 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from .models import CustomUser
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
 
-# A Registration serializer to serialize the CustomUser model for registration
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def validate_password(self, value):
-            return make_password(value)
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture', 'token']
 
     def create(self, validated_data):
-            user = get_user_model().objects.create_user(**validated_data)
-            return user
-        
+        user = get_user_model().objects.create_user(**validated_data)
+        token, created = Token.objects.get_or_create(user=user)
+        user.token = token.key
+        return user
 
-# A Login serializer 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
-    password = serializers.CharField()
+    password = serializers.CharField(required=True, write_only=True)
+    token = serializers.CharField(read_only=True)
 
     def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active():
-            return user
-        raise serializers.ValidationError("Invalid Credentials")  
+        user = authenticate(username=data['username'], password=data['password'])
+        if user and user.is_active:
+            token, created = Token.objects.get_or_create(user=user)
+            data['token'] = token.key
+            return data
+        raise serializers.ValidationError("Invalid Credentials")
 
-# # A serializer for the token retrieval 
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
-        fields = ['key'] 
-
+        fields = ['key']
 
 
 
